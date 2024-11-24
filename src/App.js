b580370d9d87f3a1a44773c1c4c4aa0a74b14317
Anjali77529 <anjalisahani840@gmail.com>
2024-11-25 00:50:09 +0530

    ih

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TopMenu from "./components/header/TopMenu";
+import MainMenu from "./components/header/MainMenu";
+// import HomePageSlider from "./components/slider/HomePageSlider";
+import OurServices from "./components/slider/OurServices";
+import TrackShipment from "./components/slider/TrackShipment";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopMenu companyName="Gravity Drive" imageUrl="images/logo.png" />
+      <MainMenu companyName="Gravity Drive" imageUrl="images/logo.png" />
+      <TrackShipment />
+
+      {/* <HomePageSlider /> */}
+      <OurServices />
+    </>
   );
 }
 
